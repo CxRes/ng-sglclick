189deba8373d62f9f12a5b29c87d73b5f84d8e96
Julien Léger <julien.leger@francetv.fr>
2015-06-19 16:35:50 +0200

    add project structure provided by yeoman generator

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngSglclickApp
+ * @description
+ * # ngSglclickApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngSglclickApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngSglclickApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngSglclickApp
+ */
+angular.module('ngSglclickApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
